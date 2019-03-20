84c0b120e4bc964cf4c8537f32921497e23da8ab
Robert Goijers <robertgoijers@macrogowrk00002.local>
2019-03-20 22:57:39 +0100

    Commit

--- activity/eftl/activity.go
+++ activity/eftl/activity.go
@@ -14,11 +14,8 @@ import (
 
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
-	"github.com/TIBCOSoftware/mashling/lib/eftl"
-	"github.com/TIBCOSoftware/mashling/lib/util"
-
-	opentracing "github.com/opentracing/opentracing-go"
-	ctx "golang.org/x/net/context"
+	"github.com/ocbitnl/flogo-components/activity/eftl/eftl"
+	"github.com/ocbitnl/flogo-components/activity/eftl/util"
 )
 
 const (
@@ -29,9 +26,6 @@ const (
 	ivUser     = "user"
 	ivPassword = "password"
 	ivCA       = "ca"
-	ivTracing  = "tracing"
-
-	ovTracing = "tracing"
 )
 
 var log = logger.GetLogger("activity-tibco-eftl")
@@ -53,34 +47,16 @@ func (a *Activity) Metadata() *activity.Metadata {
 
 // Eval implements activity.Activity.Eval
 func (a *Activity) Eval(context activity.Context) (done bool, err error) {
-	var span opentracing.Span
-	if tracing := context.GetInput(ivTracing); tracing != nil {
-		span = opentracing.SpanFromContext(tracing.(ctx.Context))
-	}
-
-	if span != nil {
-		span = opentracing.StartSpan(
-			context.TaskName(),
-			opentracing.ChildOf(span.Context()))
-		context.SetOutput(ovTracing, opentracing.ContextWithSpan(ctx.Background(), span))
-		defer span.Finish()
-	}
-
-	setTag := func(key string, value interface{}) {
-		if span != nil {
-			span.SetTag(key, value)
-		}
-	}
 
 	logError := func(format string, a ...interface{}) {
 		str := fmt.Sprintf(format, a...)
-		setTag("error", str)
 		log.Error(str)
 	}
 
 	tlsConfig := &tls.Config{
 		InsecureSkipVerify: true,
 	}
+
 	if ca, ok := context.GetInput(ivCA).(string); ok && ca != "" {
 		certificate, err := ioutil.ReadFile(ca)
 		if err != nil {
@@ -97,12 +73,15 @@ func (a *Activity) Eval(context activity.Context) (done bool, err error) {
 	options := &eftl.Options{
 		TLSConfig: tlsConfig,
 	}
+
 	if id, ok := context.GetInput(ivID).(string); ok {
 		options.ClientID = id
 	}
+
 	if user, ok := context.GetInput(ivUser).(string); ok {
 		options.Username = user
 	}
+
 	if password, ok := context.GetInput(ivPassword).(string); ok {
 		options.Password = password
 	}
@@ -110,22 +89,23 @@ func (a *Activity) Eval(context activity.Context) (done bool, err error) {
 	url := context.GetInput(ivURL).(string)
 	errorsChannel := make(chan error, 1)
 	connection, err := eftl.Connect(url, options, errorsChannel)
+
 	if err != nil {
 		logError("connection failed: %s", err)
 		return false, err
 	}
+
 	defer connection.Disconnect()
 
 	content := context.GetInput(ivContent)
 	data, err := util.Marshal(content)
+
 	if err != nil {
 		logError("failed to marshal: %s", err)
 		return false, err
 	}
 
 	if dest, ok := context.GetInput(ivDest).(string); ok {
-		setTag("dest", dest)
-		setTag("content", string(data))
 		err = connection.Publish(eftl.Message{
 			"_dest":   dest,
 			"content": data,
