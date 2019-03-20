32c40c791a3b48a36364e93577bf7c97d7c66e01
Robert Goijers <robertgoijers@macrogowrk00002.local>
2019-03-20 22:19:37 +0100

    Commit

--- vendor/github.com/TIBCOSoftware/flogo-lib/core/data/path.go
+++
@@ -1,9 +0,0 @@
-package data
-
-import (
-	"github.com/TIBCOSoftware/flogo-lib/core/mapper/exprmapper/json"
-)
-
-func PathGetValue(value interface{}, path string) (interface{}, error) {
-	return json.GetPathValue(value, path)
-}
